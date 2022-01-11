a28cc8f7207364db83b011ee426047770ffff517
Yevhenii Kurtov <yevhenii@kurtov.pro>
2022-01-11 20:10:19 +0000

    Format

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,10 +4,10 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-    .get("http://127.0.0.1:8000/health_check")
-    .send()
-    .await
-    .expect("Failed to execute request.");
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
